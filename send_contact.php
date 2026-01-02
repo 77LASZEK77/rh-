8525dfd28c1e4896b6add6c216bb8f0b900a2a08
77LASZEK77 <batlomiejlach.7@gmail.com>
2026-01-03 00:31:00 +0100

    Update send_contact.php

--- send_contact.php
+++ send_contact.php
@@ -1,32 +1,29 @@
 <?php
-session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $department = htmlspecialchars($_POST['department']);
-    $info = htmlspecialchars($_POST['info']);
-    $subject = htmlspecialchars($_POST['subject']);
-    $message = htmlspecialchars($_POST['message']);
-    $captcha = htmlspecialchars($_POST['captcha']);
-
-    if ($captcha != $_SESSION['captcha']) {
-        echo "Błędny kod potwierdzający.";
-        exit;
-    }
-
-    $to = "batlomiejlach.7@gmail.com";
-    $email_subject = "Wiadomość z formularza kontaktowego: " . $subject;
-    $email_body = "Imię i Nazwisko: $name\nE-mail: $email\nDział: $department\nInformacja dla pacjenta: $info\nTemat: $subject\nWiadomość: $message\nKod potwierdzający: $captcha";
-
-    $headers = "From: $email";
-
-    if (mail($to, $email_subject, $email_body, $headers)) {
-        echo "Wiadomość została wysłana pomyślnie.";
-    } else {
-        echo "Wystąpił błąd podczas wysyłania wiadomości.";
-    }
-} else {
-    echo "Nieprawidłowa metoda żądania.";
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    exit;
 }
-?>
 
+$name = htmlspecialchars($_POST['name'] ?? '');
+$email = htmlspecialchars($_POST['email'] ?? '');
+$department = htmlspecialchars($_POST['department'] ?? '');
+$subject = htmlspecialchars($_POST['subject'] ?? '');
+$message = htmlspecialchars($_POST['message'] ?? '');
+
+$to = "batlomiejlach.7@gmail.com";
+$email_subject = "Formularz RH+ – $subject";
+
+$email_body =
+"Imię i nazwisko: $name
+Email: $email
+Dział: $department
+
+Wiadomość:
+$message";
+
+$headers = "From: $email";
+
+if (mail($to, $email_subject, $email_body, $headers)) {
+    echo "OK";
+} else {
+    echo "ERROR";
+}
